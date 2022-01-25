1f74380c59a48172846997820724496242ac564f
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-25 21:45:54 +0530

    Added : Perform validations successfully

--- Javascript/EmployeePayrollData.js
+++ Javascript/EmployeePayrollData.js
@@ -13,7 +13,10 @@ class EmployeePayrollData {
         return this._name;
     }
     set name(name) {
-        this._name = name;
+        let nameRegex = RegExp('^[A-z]{1}[a-z]{2,}$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is incorrect';
     }
 
     get profilePic() {
@@ -55,7 +58,14 @@ class EmployeePayrollData {
         return this._startDate;
     }
     set startDate(startDate) {
-
+        let currentDate = new Date();
+        if (startDate > currentDate) {
+            throw "start date is a future date";
+        }
+        var diff = Math.abs(currentDate.getTime - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30) {
+            throw 'start date is beyond 30 days'
+        }
         this._startDate = startDate;
     }
 }
