13536c349c615087a974cd166ce9b9609d884819
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-25 21:40:23 +0530

    Added : On form submit populated the employee payroll data object successfully

---
+++ Javascript/EmployeePayrollData.js
@@ -0,0 +1,61 @@
+/** UC9 */
+class EmployeePayrollData {
+
+    //getter and setter method
+    get id() {
+        return this._id = id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+
+    get profilePic() {
+        return this._profilePic;
+    }
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._deparment;
+    }
+    set department(department) {
+        this._deparment = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get note() {
+        return this._note;
+    }
+    set note(note) {
+        this._note = note;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+
+        this._startDate = startDate;
+    }
+}
